bcebdb1cdd0427ed45b2442f66d02b4de4f98cb3
Andy Situ <and.situ@gmail.com>
2015-07-29 17:35:46 -0700

    added calculateFromI + setColor to controller

--- table.js
+++ table.js
@@ -76,5 +76,23 @@ var controller = {
 		if (ele) {
 			ele.className = "red";
 		}
+	},
+
+	calculateFromI(y, x, i) { // gives an array [y, x] of new coordinates based on direcion i and old coordinates y, x
+		switch(i) {
+			case 0: return [y, x - 1]; // left
+			case 1: return [y - 1, x]; // up
+			case 2: return [y, x + 1]; // right
+			case 3: return [y + 1, x]; // down
+			default: throw "calculateFromI only takes i from 0-3";
+		}
+	},
+
+	setColor(y, x, color) {
+		var ele = document.getElementById(y + " " + x);
+
+		if (ele) {
+
+		}
 	}
 };
