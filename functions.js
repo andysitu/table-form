fc92c52ab74f5668ec0d5a4e1f9a20dc496c7c3a
Andy Situ <and.situ@gmail.com>
2015-08-01 18:03:22 -0700

    First commit for project pages

---
+++ functions.js
@@ -0,0 +1,96 @@
+function makeTable(rows, cols) {
+	var table = document.createElement("table");
+
+	var tbody = document.createElement("tbody");
+
+	table.appendChild(tbody);
+
+	for (var i = 0; i < rows; i++) {
+		tbody.insertRow(i);
+		world.map[i] = new Array();
+
+		for (var j = 0; j <	 cols; j++) {
+			world.map[i][j] = " ";
+
+			tbody.rows[i].insertCell(j);
+			tbody.rows[i].cells[j].id = i + "_" + j;
+			tbody.rows[i].cells[j].onclick = function(e) {
+				var str = /(\d*)_(\d*)/.exec(this.id);
+				var y = Number(str[1]); 
+				var x = Number(str[2]);
+
+				controller.master("clicked", y, x);
+			}
+		}
+	}
+
+	document.body.appendChild(table);
+	table = null;
+	tbody = null;
+}
+
+function calculateFromI(y, x, i) { // gives an array [y, x] of new coordinates based on direcion i and old coordinates y, x
+	switch(i) {
+		case 0: return [y, x - 1]; // left
+		case 1: return [y - 1, x]; // up
+		case 2: return [y, x + 1]; // right
+		case 3: return [y + 1, x]; // down
+		default: throw "calculateFromI only takes i from 0-3, not " + i;
+	}
+}
+
+function funcCallFourDir(y, x, func, status) { // if sttus === true, then it'll also do function on y,x (original coordinate)
+	var coord = [0, 0];
+
+	for (var i = 0; i < 4; i++) {
+		coord = this.calculateFromI(y, x, i);
+
+		if ( coordValid(coord[0], coord[1]) ) {
+			func(coord[0], coord[1], i);
+		}
+	}
+
+	if (status === true) {
+		func(y, x);
+	}
+}
+
+function coordValid(y, x) {
+	if (y >= 0 && y < world.map.length) {
+		
+		if (x >= 0 && x < world.map[y].length) {
+			return true;
+		}
+
+	} else {
+		return false;
+	}
+}
+
+function getMapValue(y, x) {
+	if ( coordValid(y, x) ) {
+
+		var cell = document.getElementById(y + "_" + x);
+
+		return cell.innerHTML;
+
+	} else {
+		throw("Invalid coordinates for getMapValue: " + y + " " + x);
+	}
+}
+
+function setMapValue(y, x, value) {
+	if ( coordValid(y, x) ) {
+		var oldValue = getMapValue(y, x);
+
+		var cell = document.getElementById(y + "_" + x);
+
+		if (value !== oldValue) {
+			cell.innerHTML = value;
+		}
+
+
+	} else {
+		throw("Invalid coordinates for setMapValue: " + y + " " + x);
+	}
+}
---
+++ table.js
@@ -0,0 +1,219 @@
+var world = {
+	map: [],
+	playerLoc: [0,0],
+
+	classTranslator(value) { // give it a letter (ex: "r") to get back the right color/ class ("red"). Also, can work in reverse
+		if (value.length <= 1) {
+			switch(value) { // NOTE: ALSO UPDATE ELSE CASE AND VALUETRANSLATOR
+				case "r": return "red";
+				case "g": return "green";
+				default: console.log("classTranslator: " + value);
+						 throw("Unknown value given to classTranslator");
+			}
+		} else {
+			switch(value) {
+				case "red": return "r";
+				case "green": return "g";
+				default: console.log("classTranslator: " + value);
+						 throw("Unknown value given to classTranslator");
+			}
+		}
+	},
+	changeMap(y, x, colorValue) { // Changes the value in map array and appends the corresponding class in the table-cell
+		if ( coordValid(y, x) ) {
+			if (this.map[y][x] === " ") {
+				this.map[y][x] = colorValue;
+
+				var ele = document.getElementById(y + "_" + x);
+				ele.className = this.classTranslator(colorValue);
+
+				return true;
+			}
+		} else {
+			return false;
+		}
+
+	},
+
+	calculate(y, x) { // will calculate the points of that current position
+							 // color is for theoretical calculation if something were placed there
+		var sum = 0;
+
+		funcCallFourDir(y, x, function(y1, x1) {
+			sum += world.valueTranslator(y1, x1);
+		})
+
+		sum += world.valueTranslator(y, x);
+
+		return sum;
+	},
+
+	valueTranslator(y, x) {
+		switch(world.map[y][x]) {
+			case "r": return -1;
+			case "g": return 1;
+			case " ": return 0;
+			default: throw("Error in valueTranslator " + this.map[y][x]);
+		}	
+	}
+
+
+};
+
+var blocks = {
+	controller(y, x) { // handles what to do with destroying blocks, etc depending on their values.
+		
+		funcCallFourDir(y, x, function(y1, x1) {
+			if (world.valueTranslator(y1, x1) >= 1) {
+				var value = world.calculate(y1, x1);
+
+				setMapValue(y1, x1, value);
+			}
+		}, true ); // true so that controller also acts on y, x
+	}
+}
+
+function init() {
+	var cols = Math.floor((window.innerWidth / 10) * 0.80);
+	var rows = Math.floor(window.innerHeight / 10 - 2);
+
+	makeTable(rows, cols);
+
+	controller.master("player")
+};
+
+window.onload = init;
+
+var controller = {
+	master(status, y, x) {
+		if (status === "clicked") {
+			this.clicked(y,x);
+			red.response(y, x);
+			blocks.controller(y,x);
+			red.testIfSurrounded(y, x);
+
+		} else if (status === "player") {
+
+		}
+	},
+
+	clicked(y, x) {
+		var ele = document.getElementById(y + "_" + x);
+
+		world.changeMap(y, x, "g") // appends "g" onto the map where player clicked
+
+	}
+	
+};
+
+var red = {
+	greenBlocks(y, x) {
+		var sum = 0;
+
+		funcCallFourDir(y, x, function(y1, x1) {
+			if (world.valueTranslator(y1, x1) > 0) {
+				sum += world.valueTranslator(y1, x1);
+			}
+		})
+
+		sum += world.valueTranslator(y, x);
+
+		return sum;
+	},
+
+	bestResponse(y, x) { // checks for where block to place to be next to greatest value of green blocks
+		var sums = {};
+		var max = -100;
+
+		funcCallFourDir(y, x, function(y1, x1, i) {
+
+			if (world.map[y1][x1] === ' ') {
+
+				sums[i] = red.greenBlocks(y1, x1);
+
+				max = Math.max(max, sums[i]);
+
+			}
+
+		}, false);
+
+		var maxI = [];
+
+		for (var key in sums) {
+			if (sums[key] === max) {
+				maxI.push(key);
+			}
+		}
+
+		if (maxI.length >= 1) {
+			var randomIndex = Math.floor(maxI.length * Math.random());
+			return Number(maxI[randomIndex]);
+		} else { // returns false if none of 4 directions have an empty space
+			return false;
+		}
+	},
+
+	response(y, x) { // uses bestResponse to check for block placement. if it returns false, then place random block on map
+		var dir = this.bestResponse(y, x);
+
+		if (dir !== false) {
+
+			var coord = calculateFromI(y, x, dir);
+
+			world.changeMap(coord[0], coord[1], "r");
+
+			return true;
+
+		} else {
+
+			var count = 0, max = world.map.length * world.map[0].length;
+			while ( count < max) {
+				y = Math.floor(Math.random() * world.map.length);
+				x = Math.floor(Math.random() * world.map[y].length);
+
+				var bool = world.changeMap(y, x, "r");
+
+				if ( bool ) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	},
+
+
+	greenList: [],
+
+	notSurrounded(y, x) { // returns true if block in y,x has an empty block surrounding it
+		var status = false;
+
+		funcCallFourDir(y, x, function(y1, x1) {
+			if (world.map[y1][x1] === " ") {
+				status = true;
+			}
+		})
+
+		return status;
+
+	},
+
+	testIfSurrounded(y, x) { // runs notSurrounded in the 4 spaces surrounded y,x and adds to list if true, but also removes it from list if it becomes notSurrounded
+		funcCallFourDir(y, x, function(y1, x1) {
+			if (world.valueTranslator(y1, x1) >= 1) {
+				var index = red.greenList.indexOf(y1 + "_" +x1);
+
+				if ( index === -1 ) {
+					if ( red.notSurrounded(y1, x1) ) {
+						red.greenList.push(y1 + "_" +x1);
+					}
+				} else {
+					if ( !red.notSurrounded(y1, x1) ) {
+						red.greenList.splice(index, 1);
+					}
+				}
+			}
+
+		}, true)
+	}
+}
