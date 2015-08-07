8ff7f4ae218c844d51e7abe94db85736a1596ecd
Andy Situ <and.situ@gmail.com>
2015-08-06 17:04:15 -0700

    empty cells have value 0, not ' '

--- functions.js
+++ functions.js
@@ -12,13 +12,13 @@ function makeTable(rows, cols) {
 		world.map[i] = new Array();
 
 		for (var j = 0; j <	 cols; j++) {
-			world.map[i][j] = " ";
+			world.map[i][j] = 0;
 
 			tbody.rows[i].insertCell(j);
 			tbody.rows[i].cells[j].id = i + "_" + j;
 		}
 	}
-	
+
 	world.totalCells = i * j;
 
 	document.body.appendChild(table);
